f0975ce60e3eb7ed142fc6123ca90f0d745cb7d9
Daniel Boll <danielboll.academico@gmail.com>
2021-04-03 22:18:24 -0300

    Adding git status command

---
+++ src/commands/git.js
@@ -0,0 +1,19 @@
+module.exports = (client, tokens) => {
+  if (tokens.length == 0) {
+    client.action("namelessblossom", "https://github.com/Daniel-Boll");
+  } else {
+    const [page] = tokens;
+
+    if (page.toLowerCase() == "lambdaio") {
+      client.action(
+        "namelessblossom",
+        "https://github.com/Daniel-Boll/LambdaIO"
+      );
+    } else if (page.toLowerCase() == "chatbot") {
+      client.action(
+        "namelessblossom",
+        "https://github.com/Daniel-Boll/TwitchChatBot"
+      );
+    }
+  }
+};
