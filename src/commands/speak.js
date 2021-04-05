f1e78f7cafd5f59460f5c20dce4abad4546c0721
Daniel Boll <danielboll.academico@gmail.com>
2021-04-05 20:03:26 -0300

    Adding tts to sound at !falar

---
+++ src/commands/speak.js
@@ -0,0 +1,18 @@
+const { client } = require("../config/client");
+const { exec } = require("child_process");
+
+module.exports = (tokens) => {
+  client.action("namelessblossom", "Irei falar, aguarde!");
+
+  const lang = tokens.shift();
+  const phrase = tokens;
+
+  console.log(phrase);
+
+  exec(
+    `powershell.exe -File "src/scripts/run.ps1" -Lang ${lang} -Phrase ${phrase}`,
+    (_, stdout, ___) => {
+      console.log(stdout);
+    }
+  );
+};
