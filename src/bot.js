967e2df02f9e229f7d7803aa0ed05e3a134a8228
Daniel Boll <danielboll.academico@gmail.com>
2021-04-05 23:38:30 -0300

    Changing in bot

--- src/bot.js
+++ src/bot.js
@@ -3,8 +3,8 @@ const { channels } = require("./config/constants");
 const commandHandler = require("./command");
 
 client.on("connected", (address, port) => {
-  // Eventually stop this.
-  // Only mantain for debug purpouses.
+  // Eventually stop this.  Only mantain for debug
+  // purpouses.
   client.action(channels[0], "I'm here, beep boop 🤖");
 });
 
